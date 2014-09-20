c5f9702f4628c4bbfc6ad9703348404f905191a9
Theo <theostanton@gmail.com>
2014-09-20 09:02:15 +0100

    merged connectivity and frame manager

--- dial.py
+++ dial.py
@@ -1,78 +1,84 @@
-import pygame
 import numpy as np
-from colors import *
+import pygame
 from pygame import gfxdraw
+
+from Element import Element
+from colors import *
+import Data
+
+
 gfx = pygame.gfxdraw
 draw = pygame.draw
 
+pygame.init()
+font = pygame.font.Font(None, 20)
 
+# Axis ids
 ROLL = 0
 PITCH = 1
 YAW = 2
 
-pygame.init()
-font = pygame.font.Font(None, 20, bold=False)
 
-class Dial():
-
-    def __init__(self,x,y,r,axisid,angleids, label):
+class Dial(Element):
+    def __init__(self, surface, axisid, angleids, label):
+        super(Dial, self).__init__(surface, label)
         self.axisid = axisid
         self.angleids = angleids
-        self.label = label
-        pad = 10
-        #self.rect = pygame.Rect(x,y,w,h)
-        self.x = x + pad
-        self.cx = x + r
-        self.y = y + pad
-        self.cy = y + r
-        self.r = r - 2 * pad
 
-    def draw(self,surface, angles, motors):
+        # self.angles = {}
+        # for a in 'AGMDE':
+        # self.angles[a] = [0,0,0]
 
-        gfx.line(surface, self.cx - self.r,self.cy,self.cx + self.r, self.cy,white)
+    def draw(self):
+        if not self.set:
+            return
 
-        ren = font.render(self.label,True,white)
+        cx = self.rect.centerx
+        cy = self.rect.centery
 
-        surface.blit(ren,(self.cx-ren.get_width() / 2,self.cy - self.r / 2))
+        gfx.filled_circle(self.surface, cx, cy, self.r, gray_10)
 
-        for a in range(0,179,45):
-            dx = int( self.r * np.sin( np.radians(  a + 90) ) )
-            dy = int( self.r * np.cos( np.radians(  a + 90) ) )
-            gfx.line( surface, self.cx - dx, self.cy - dy, self.cx + dx, self.cy + dy, white_40 )
-            #draw.line( surface, (255,255,255,1), (self.cx - dx, self.cy - dy), (self.cx + dx, self.cy + dy) )
+        gfx.line(self.surface, self.x, cy, self.rect.right, cy, white)
 
-        for angleid  in self.angleids:
+        # label after fill
+        super(Dial, self).draw()
 
-            dx = int( self.r * np.sin( np.radians( angles[angleid][self.axisid] + 90 ) ) )
-            dy = int( self.r * np.cos( np.radians( angles[angleid][self.axisid] + 90 ) ) )
+        for a in range(0, 179, 45):
+            dx = int(self.r * np.sin(np.radians(a + 90)))
+            dy = int(self.r * np.cos(np.radians(a + 90)))
+            gfx.line(self.surface, cx - dx, cy - dy, cx + dx, cy + dy, white_40)
 
-            lx = self.cx - dx
-            ly = self.cy - dy
-            rx = self.cx + dx
-            ry = self.cy + dy
+        for angleid in self.angleids:
 
-            #draw.aaline( surface, colors[angleid], (lx,ly),(rx,ry) )
-            gfx.line( surface, lx,ly,rx,ry, colors[angleid] )
+            dx = int(self.r * np.sin(np.radians(Data.angles[angleid][self.axisid] + 90)))
+            dy = int(self.r * np.cos(np.radians(Data.angles[angleid][self.axisid] + 90)))
+
+            lx = cx - dx
+            ly = cy - dy
+            rx = cx + dx
+            ry = cy + dy
+
+            gfx.line(self.surface, lx, ly, rx, ry, colors[angleid])
 
             if angleid is 'M':
                 scale = 5
-                bars = [0,0,0,0]
+                bars = [0, 0, 0, 0]
 
                 if self.axisid is PITCH:
-                    bars[0] = scale * motors['a'].tot
-                    bars[1] = scale * motors['b'].tot
-                    bars[2] = scale * motors['c'].tot
-                    bars[3] = scale * motors['d'].tot
+                    bars[0] = Data.pid['a'][3] * 10
+                    bars[1] = Data.pid['b'][3] * 10
+                    bars[2] = Data.pid['c'][3] * 10
+                    bars[3] = Data.pid['d'][3] * 10
 
-                else :
-                    bars[0] = scale * motors['b'].tot
-                    bars[1] = scale * motors['d'].tot
-                    bars[2] = scale * motors['a'].tot
-                    bars[3] = scale * motors['c'].tot
+                else:
+                    bars[0] = Data.pid['a'][3] * 10
+                    bars[1] = Data.pid['c'][3] * 10
+                    bars[2] = Data.pid['b'][3] * 10
+                    bars[3] = Data.pid['d'][3] * 10
 
-                draw.line(surface, (255,0,0), (lx - 2, ly) , ( lx - 2, ly+bars[0]), 5)
-                draw.line(surface, (255,0,0), (lx + 2, ly) , ( lx + 2, ly+bars[1]), 5)
-                draw.line(surface, (255,0,0), (rx - 2, ry) , ( rx - 2, ry+bars[2]), 5)
-                draw.line(surface, (255,0,0), (rx + 2, ry) , ( rx + 2, ry+bars[3]), 5)
+                draw.line(self.surface, (255, 0, 0), (lx - 2, ly), ( lx - 2, ly + bars[0]), 2, )
+                draw.line(self.surface, (255, 0, 0), (lx + 2, ly), ( lx + 2, ly + bars[1]), 2, )
+                draw.line(self.surface, (255, 0, 0), (rx - 2, ry), ( rx - 2, ry + bars[2]), 2, )
+                draw.line(self.surface, (255, 0, 0), (rx + 2, ry), ( rx + 2, ry + bars[3]), 2, )
 
-        gfx.aacircle(surface, self.cx, self.cy, self.r, white)
+        gfx.aacircle(self.surface, cx, cy, self.r, white)
